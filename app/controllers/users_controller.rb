d837d53678f3c9d723f2020afa181a354e67ad71
Mark Mullan <mark.mullan@clover.com>
2020-04-12 11:24:41 -0700

    add endpoint to update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
-class UsersController < ApplicationController
+class UsersController < ApiController
   def show
     render json: current_user
+  end
+
+  def update
+    @user = User.find_by!(id: current_user.id)
+
+    @user.assign_attributes(user_params)
+
+    if @user.save
+      return render json: @user
+    end
+
+    render json: @user.errors, status: :bad_request
+  end
+
+  def user_params
+    params
+      .permit(:phone_number, :first_name, :last_name)
   end
 end
