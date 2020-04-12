6f03491b021cb8f1061ad98fab32c63a202dd10a
Mark Mullan <mark.mullan@clover.com>
2020-04-12 15:18:05 -0700

    exclude null values from api responses for easier JS destructuring

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def as_json(options = {})
+    super(options)
+      .reject { |k, v| v.nil? } # exclude `null` values from JSON to allow for easier destructuring
+  end
 end
