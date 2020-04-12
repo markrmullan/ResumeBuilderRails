1ffba137e2bd64978501816e67ff42369f6a6bd9
Mark Mullan <mark.mullan@clover.com>
2020-04-12 15:34:06 -0700

    actually, need to recursively remove null values

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,17 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def as_json(options = {})
-    super(options)
-      .reject { |k, v| v.nil? } # exclude `null` values from JSON to allow for easier destructuring
+    recursive_remove_empty_and_nil_values(super(options))
+  end
+
+  # exclude `null` values from JSON to allow for easier destructuring
+  def recursive_remove_empty_and_nil_values(hash_or_array)
+    p = proc do |*args|
+      v = args.last
+      v.delete_if(&p) if v.respond_to? :delete_if
+      v.nil? || v.respond_to?(:"empty?") && v.empty?
+    end
+
+    hash_or_array.delete_if(&p)
   end
 end
