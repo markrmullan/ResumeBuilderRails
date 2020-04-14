fa509516ccd99ef56524356264cbccfcdc17314a
Mark Mullan <mark.mullan@clover.com>
2020-04-14 15:42:09 -0700

    try removing localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,13 @@
 # https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'http://localhost:3000'
-    resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head],
-             credentials: true
-  end
+  # allow do
+  #   origins 'http://localhost:3000'
+  #   resource '*',
+  #            headers: :any,
+  #            methods: %i[get post put patch delete options head],
+  #            credentials: true
+  # end
 
   allow do
     origins 'https://resume-builder-ts.herokuapp.com/'
