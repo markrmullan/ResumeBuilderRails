3a55c24c8320ee1db67428c6ba7200beaa709c5f
Mark Mullan <mark.mullan@clover.com>
2020-04-14 15:38:29 -0700

    try try again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,15 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'https://resume-builder-ts.herokuapp.com/']
+    origins 'http://localhost:3000'
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
+  end
+
+  allow do
+    origins 'https://resume-builder-ts.herokuapp.com/'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
