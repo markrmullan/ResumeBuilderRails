f440d763c0a32e800bbaf0993efaa1db6de2f5cc
Mark Mullan <mark.mullan@clover.com>
2020-04-14 15:52:56 -0700

    do not csrf verify user sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,5 @@ class RegistrationsController < Devise::RegistrationsController
   # devise by default disallows some operations (sign up, login) if the user already has an active session.
   # this line removes that default constraint
   skip_before_action :require_no_authentication
+  skip_before_action :verify_authenticity_token
 end
