a4ea92a8c7c76216d6cba2ffcbb840c09fcd74b3
Mark Mullan <mark.mullan@clover.com>
2020-05-15 18:03:49 -0700

    try try again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  protect_from_forgery with: :exception
+  protect_from_forgery unless: -> { request.format.json? }
 
   # originally for Devise, but we want to allow all APIs to respond to, and only to, application/json requests
   clear_respond_to
--- app/controllers/passwords_controller.rb
+++
@@ -1,3 +0,0 @@
-class PasswordsController < Devise::PasswordsController
-  skip_before_action :verify_authenticity_token
-end
